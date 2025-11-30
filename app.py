6c216c0620a7ffce760a0d538ea3f05de7910db8
Muge <mugekiziltas@gmail.com>
2025-11-30 23:51:50 +0300

    Mesaji Turkceye cevirdim

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello! I am running inside a Docker container!"
+    return "Merhaba! BU benim ilk CI/CD projem!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
